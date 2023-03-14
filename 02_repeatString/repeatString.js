b23c2261595b3ea2d2acd6d985a0df72903a58c2
Guy Hennequin <guy.a.hennequin@gmail.com>
2023-03-14 11:01:21 +0900

    Complete exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
+/* ## Hints
+- Take note of the above function call- how exactly is it being called?
+- You're going to want to use a loop for this one.
+- Create a variable to hold the string you're going to return, 
+    create a loop that repeats the given number of times and add the given string to the result on each loop.*/
 
-};
 
-// Do not edit below this line
+const repeatString = function (word, times) {
+    if (times < 0) return "ERROR";
+    let string = "";
+    for (let i = 0; i < times; i++) {
+      string += word;
+    }
+    return string;
+  };
+  
+// Do not remove
 module.exports = repeatString;
